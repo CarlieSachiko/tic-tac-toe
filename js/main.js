f962d598e2623852ece82dfafa863a48306ce310
Carlie Clippinger <carliesachiko@gmail.com>
2016-12-04 11:23:43 -0800

    Created layout

---
+++ js/main.js
@@ -0,0 +1,26 @@
+/*--- variables ---*/
+var restart;
+var winner;
+var player;
+var board=document.querySelector('table');
+var gameOver;
+
+
+/*--- event listners ---*/
+
+
+
+
+
+
+
+
+/*--- functions ---*/
+// restart function (){
+//   player='';
+// };
+
+
+
+
+
